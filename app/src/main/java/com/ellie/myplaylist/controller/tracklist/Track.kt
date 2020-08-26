7ac9265f4059bf36d53ff5642a4775ba1ee700bb
foreknowledge <kyj_0129@naver.com>
2020-08-26 19:16:22 +0900

    feat: Track 구조 변경

--- app/src/main/java/com/ellie/myplaylist/controller/tracklist/Track.kt
+++ app/src/main/java/com/ellie/myplaylist/controller/tracklist/Track.kt
@@ -3,5 +3,8 @@ package com.ellie.myplaylist.controller.tracklist
 data class Track(
     val number: Int,
     val title: String,
-    val playTime: String
+    val artist: String,
+    val playTime: String = "00:00",
+    val lyrics: String? = null,
+    val introduction: String? = null
 )
