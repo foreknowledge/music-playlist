cdc33b9745bbc50041249d05cc5b50a52315f043
foreknowledge <kyj_0129@naver.com>
2020-08-27 12:19:43 +0900

    refactor: Activity에 view binding 적용

--- app/src/main/java/com/ellie/myplaylist/MainActivity.kt
+++ app/src/main/java/com/ellie/myplaylist/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.ellie.myplaylist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bluelinelabs.conductor.ChangeHandlerFrameLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
-import com.ellie.myplaylist.controller.*
+import com.ellie.myplaylist.controller.PlaylistController
+import com.ellie.myplaylist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var router: Router
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val container: ChangeHandlerFrameLayout = findViewById(R.id.container)
-
-        router = Conductor.attachRouter(this, container, savedInstanceState)
+        router = Conductor.attachRouter(this, binding.container, savedInstanceState)
         if (!router.hasRootController()) {
             router.setRoot(RouterTransaction.with(PlaylistController()))
         }
