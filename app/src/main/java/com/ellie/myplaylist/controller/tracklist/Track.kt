aea4e475c3305bceea2d8c9326c40d40856341e9
foreknowledge <kyj_0129@naver.com>
2020-08-27 18:12:49 +0900

    feat: Track 데이터에 Moshi 적용

--- app/src/main/java/com/ellie/myplaylist/controller/tracklist/Track.kt
+++ app/src/main/java/com/ellie/myplaylist/controller/tracklist/Track.kt
@@ -1,9 +1,22 @@
 package com.ellie.myplaylist.controller.tracklist
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Track(
+    @field:Json(name = "title")
     val title: String,
+
+    @field:Json(name = "artist")
     val artist: String,
+
+    @field:Json(name = "play_time")
     val playTime: String = "00:00",
+
+    @field:Json(name = "lyrics")
     val lyrics: String? = null,
+
+    @field:Json(name = "introduction")
     val introduction: String? = null
 )
