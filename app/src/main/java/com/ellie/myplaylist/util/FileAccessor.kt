7f4d333df20bd794511b0c8975317922ca154e87
foreknowledge <kyj_0129@naver.com>
2020-08-27 19:46:14 +0900

    feat: json 파일에 접근하는 클래스 구현 (임시 샘플데이터 포함)

---
+++ app/src/main/java/com/ellie/myplaylist/util/FileAccessor.kt
@@ -0,0 +1,58 @@
+package com.ellie.myplaylist.util
+
+import android.content.Context
+import androidx.annotation.WorkerThread
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class FileAccessor(private val context: Context, private val fileName: String) {
+    @WorkerThread
+    fun readData(): String {
+        try {
+            context.openFileInput(fileName).use { inputStream ->
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                return reader.readLines().joinToString("")
+            }
+        } catch (e: IOException) {
+            writeData(sampleData)
+            return sampleData
+        }
+    }
+
+    @WorkerThread
+    fun writeData(data: String) {
+        // 해당 파일 있으면 열고, 없으면 생성한 뒤 data를 쓴다.
+        OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE)).use {
+            it.write(data)
+        }
+    }
+
+    companion object {
+        private val sampleData = """
+            [
+              {
+                "title": "All We Got (feat. Kanye West &amp; Chicago Children\\'s Choir)",
+                "artist": "Chance the Rapper",
+                "play_time": "03:23"
+              },
+              {
+                "title": "No Problem (feat. Lil Wayne and 2 Chainz)",
+                "artist": "Chance the Rapper",
+                "play_time": "05:04"
+              },
+              {
+                "title": "Summer Friends (feat. Jeremih and Francis and the Lights)",
+                "artist": "Chance the Rapper",
+                "play_time": "04:50"
+              },
+              {
+                "title": "D.R.A.M. Sings Special",
+                "artist": "Chance the Rapper",
+                "play_time": "01:41"
+              }
+            ]
+        """.trimIndent()
+    }
+}
